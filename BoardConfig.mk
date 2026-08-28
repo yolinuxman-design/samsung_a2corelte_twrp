4b8c1aed97ae8a84bd844181e09e4fafa65bd40f
yolinuxman-design <yolinuxman@gmail.com>
2026-08-28 18:05:26 +0545

    a2core: add missing contexts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,7 @@ BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x002800000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/samsung/a2corelte/prebuilt/zImage
-TARGET_PREBUILT_DTB := device/samsung/a2corelte/prebuilt/dtb.img
+TARGET_PREBUILT_DTB := device/samsung/a2corelte/prebuilt/dt.img
 
 # Use this flag if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
